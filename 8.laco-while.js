a656d5ffdd85740aafad8c0eb4ba985a9b5169e5
vladimir <vladimir.menezes@ba.estudante.senai.br>
2026-03-05 16:46:53 -0300

    Muitas coisas

---
+++ 8.laco-while.js
@@ -0,0 +1,8 @@
+// Laço de repeticao^: Whie
+
+let i = 1
+
+while (i <= 5) {
+    console.log(i)
+    i++
+}
---
+++ 9.operadores-I.js
@@ -0,0 +1,20 @@
+// operacoes logicos
+
+let idade = 10
+let peso = "10"
+
+console.log(" - COMPARAÇÃO")
+console.log("comparando conteudo")
+console.log(idade == peso)
+
+console.log("\nComparano tipo e conteudo")
+console.log(idade ===peso)  
+
+console.log(" - DIFERENÇA")
+console.log("\Ncomparando conteudo")
+console.log("IDADE E PESO SAO DIFERNTES?")
+console.log(idade != peso)
+
+console.log("\Ncomparando tipos e conteudo")
+console.log("IDADE E PESO SAO DIFERNTES?")
+console.log(idade !== peso)
