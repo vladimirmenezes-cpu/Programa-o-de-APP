490dc323555269a2558ba3a58bed444ee30e481d
vladimir <vladimir.menezes@ba.estudante.senai.br>
2026-03-05 14:43:49 -0300

    Switch-case

---
+++ 6.switch-case.js
@@ -0,0 +1,22 @@
+let opcao = 3
+
+switch (opcao) {
+    case 1:
+        console.log("opção 1")
+        break;
+    case 2:
+        console.log("opção 2")
+        break;
+    case 3:
+        console.log("opção 3")
+        break;
+    case 4:
+        console.log("opção 4")
+        break;
+    case 5:
+        console.log("opção 5")
+        break;
+
+    default:
+        break;
+}
