d3c4b4262b43ae75354f359a5bbcc4da55ae012d
vladimir <vladimir.menezes@ba.estudante.senai.br>
2026-03-26 16:54:26 -0300

    React

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,14 @@
+
+import './style.css'
+
+function Mensagem() {
+    return(
+        // Comandos HTML.
+        <div className='mensagem'>
+            <h1>Componente</h1>
+            <p>Estes é um componente com uma mensagem simples</p>
+        </div>
+    )
+}
+
+export default Mensagem
